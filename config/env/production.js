e9a6f9962b7b632fd6235770c6f7c0ff41e53fa6
Nguyen Loc <loc9493@gmail.com>
2022-01-12 14:37:56 +0700

    [IOS-3578]:Add missing localize

--- config/env/production.js
+++ config/env/production.js
@@ -352,7 +352,7 @@ module.exports = {
   * this, just try deploying without setting it and see if it works.)       *
   *                                                                         *
   ***************************************************************************/
-  // port: 80,
+  port: 5000,
 
 
 
